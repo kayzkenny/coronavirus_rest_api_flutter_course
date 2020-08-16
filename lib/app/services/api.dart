0eb4a82f51be940816fa273fcc815d11ca3a444e
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-08-16 14:42:42 +0100

    test getAccessToken()

--- lib/app/services/api.dart
+++ lib/app/services/api.dart
@@ -2,10 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/services/api_keys.dart';
 
 class API {
+  API({@required this.apiKey});
   final String apiKey;
   static final String host = 'ncov2019-admin.firebaseapp.com';
-
-  API({@required this.apiKey});
 
   factory API.sandbox() => API(apiKey: APIKeys.ncovSandboxKey);
 
