42a8734b17f6a0239d308eedab8f1d55d58a2e97
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-08-17 09:31:55 +0100

    Create data repository class

---
+++ lib/app/repositories/data_repository.dart
@@ -0,0 +1,16 @@
+import 'package:coronavirus_rest_api_flutter_course/app/services/api.dart';
+import 'package:coronavirus_rest_api_flutter_course/app/services/api_service.dart';
+import 'package:flutter/cupertino.dart';
+
+class DataRepository {
+  DataRepository({@required this.apiService});
+  final APIService apiService;
+
+  Future<int> getEndpointData(Endpoint endpoint) async {
+    final accessToken = await apiService.getAccessToken();
+    return await apiService.getEndpointData(
+      accessToken: accessToken,
+      endpoint: endpoint,
+    );
+  }
+}
