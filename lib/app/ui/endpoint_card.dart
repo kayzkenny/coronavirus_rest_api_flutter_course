1737caeb8b845b7a60f98c2e5ec48c23a2e704ff
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-08-18 13:25:02 +0100

    update dashboard with list view

--- lib/app/ui/endpoint_card.dart
+++ lib/app/ui/endpoint_card.dart
@@ -1,8 +1,34 @@
+import 'package:coronavirus_rest_api_flutter_course/app/services/api.dart';
 import 'package:flutter/material.dart';
 
 class EndpointCard extends StatelessWidget {
+  final Endpoint endpoint;
+  final int value;
+
+  const EndpointCard({Key key, this.endpoint, this.value}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Cases',
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              Text(
+                value != null ? value.toString() : '',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
