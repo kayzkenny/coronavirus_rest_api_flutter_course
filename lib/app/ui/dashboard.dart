ef2d291c4c726a99d61d3ebbe3bb62b688da49bb
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-08-20 13:03:36 +0100

    create alert dialog

--- lib/app/ui/dashboard.dart
+++ lib/app/ui/dashboard.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:coronavirus_rest_api_flutter_course/app/repositories/data_repository.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/repositories/endpoints_data.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/services/api.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/ui/endpoint_card.dart';
 import 'package:coronavirus_rest_api_flutter_course/app/ui/last_updated_status_text.dart';
+import 'package:coronavirus_rest_api_flutter_course/app/ui/show_alert_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -15,10 +18,27 @@ class _DashboardState extends State<Dashboard> {
   EndpointsData _endpointsData;
 
   Future<void> _updateData() async {
-    final dataRepository = Provider.of<DataRepository>(context, listen: false);
-    final endpointsData = await dataRepository.getAllEndpointsData();
+    try {
+      final dataRepository =
+          Provider.of<DataRepository>(context, listen: false);
+      final endpointsData = await dataRepository.getAllEndpointsData();
 
-    setState(() => _endpointsData = endpointsData);
+      setState(() => _endpointsData = endpointsData);
+    } on SocketException catch (_) {
+      showAlertDialog(
+        context: context,
+        title: 'Connection Error',
+        content: 'Could not retrieve data. Please try again later.',
+        defaultActionText: 'OK',
+      );
+    } catch (_) {
+      showAlertDialog(
+        context: context,
+        title: 'Unknown Error',
+        content: 'Please contact support or try again later.',
+        defaultActionText: 'OK',
+      );
+    }
   }
 
   @override
